87fb776cf37a10644ae3cf066feee3d7d14ebc35
Wagner B <wagner_mbarbosa@hotmail.com>
2022-02-03 18:49:04 -0300

    Exercicios de 1 a 6

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/testePositivoNegativo.js
@@ -0,0 +1,11 @@
+let numeroTeste = -50;
+
+if (numeroTeste != 0)
+{
+    if (numeroTeste > 0)
+        console.log("positive");
+    else
+        console.log("negative");
+}
+else
+    console.log("zero");
