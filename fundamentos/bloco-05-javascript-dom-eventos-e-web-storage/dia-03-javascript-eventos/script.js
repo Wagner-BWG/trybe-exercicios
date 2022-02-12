269414e2853fed2b720f3c5bd432c6c989b49bcd
Wagner B <wagner_mbarbosa@hotmail.com>
2022-02-12 18:48:32 -0300

    Ex1, 2 and 3 done

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -1,3 +1,4 @@
+
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
@@ -13,4 +14,56 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  // Exercício 1.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function createDaysOfTheMonth(monthDays){
+      const monthDaysList = document.getElementById("days");
+      
+      for (let i = 0; i < monthDays.length; i += 1){
+          const monthDayElement = document.createElement("li");
+          monthDayElement.innerText = monthDays[i];
+          monthDayElement.className = "day";
+          if ((i-1) === 24 || (i-1) === 25 || (i-1) === 31){
+            monthDayElement.className += " holiday";
+          }
+          if ((i-1) === 4 ||  (i-1) === 11 || (i-1) === 18 || (i-1) === 25){
+            monthDayElement.className += " friday";
+          }
+
+          monthDaysList.appendChild(monthDayElement);
+      }
+  }
+  createDaysOfTheMonth(dezDaysList);
+
+  // Exercício 2.
+
+  function createHolidayButton(feriados){
+      const button = document.createElement("button");
+      button.id = "btn-holiday";
+      button.innerText = feriados;
+      document.getElementsByClassName("buttons-container")[0].appendChild(button);
+  }
+
+  createHolidayButton("Feriados");
+
+  // Exercício 3.
+
+  function addDisplayHolidays(){
+      const button = document.getElementById("btn-holiday");
+      button.addEventListener("click", displayHolidays);
+  }
+  addDisplayHolidays();
+
+  function displayHolidays(){
+      const holidays = document.getElementsByClassName("holiday");
+      for (let i = 0; i < holidays.length; i += 1){
+          if (holidays[i].style.backgroundColor != "red"){
+            holidays[i].style.backgroundColor = "red";
+          }
+          else {
+            holidays[i].style.backgroundColor = "rgb(238,238,238)";
+          }
+      }
+  }
