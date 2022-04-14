6b06cbb756ebe02b109a78e297c143b9386ab9a3
Wagner B <wagner_mbarbosa@hotmail.com>
2022-04-14 15:03:46 -0300

    Limpeza geral

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/Clock.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Clock extends Component {
-  render() {
-    return <h2>{setInterval(new Date().toLocaleTimeString('pt', { hour12: true }), 1000)}</h2>;
-  }
-}
-
-export default Clock;
